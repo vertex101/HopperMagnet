20bae5f382c8c3db046effe36a0e35afa810686b
Vertex101 <vertex101@gmx.com>
2020-08-01 22:13:32 -0400

    added more code
    got the magnet part of hopper working but need to refine it

--- src/main/java/xyz/vertex101/hoppermagnet/HopperMagnet.java
+++ src/main/java/xyz/vertex101/hoppermagnet/HopperMagnet.java
@@ -1,10 +1,23 @@
 package xyz.vertex101.hoppermagnet;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.vertex101.hoppermagnet.items.ItemHopMagnet;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public final class HopperMagnet extends JavaPlugin {
     public static double maxRange = 8.0D;
+
+    public final List<Block> magnetBlocks = new ArrayList<Block>();
 
     public static HopperMagnet getPlugin() {
         return JavaPlugin.getPlugin(HopperMagnet.class);
@@ -16,11 +29,49 @@ public final class HopperMagnet extends JavaPlugin {
         this.maxRange = getConfig().getInt("max-range");
         ItemHopMagnet.magnetHopper();
 
+        getServer().getPluginManager().registerEvents(new ItemHopMagnet(), this);
+
+        ItemSearch itemSearch = new ItemSearch();
+        getServer().getScheduler().scheduleSyncRepeatingTask((Plugin)this, itemSearch, 5L, 5L);
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public class ItemSearch implements Runnable {
+        private ItemSearch() {}
+
+        public void run() {
+            for (World world : HopperMagnet.getPlugin().getServer().getWorlds()) {
+                for (Entity entity : world.getEntities()) {
+                    if (!(entity instanceof Item))
+                        continue;
+                    Item item = (Item)entity;
+                    ItemStack stack = item.getItemStack();
+                    Location location = item.getLocation();
+                    if (stack.getAmount() <= 0 || item.isDead() || item.getPickupDelay() > item.getTicksLived())
+                        continue;
+                    Block closestBlock = null;
+                    double distanceSmall = HopperMagnet.maxRange;
+                    for (Block block : magnetBlocks) {
+                        if (block != null)
+                            if (block.getWorld().getName().equals(world.getName())) {
+                                double playerDistance = block.getLocation().distance(location);
+                                if (playerDistance < distanceSmall) {
+                                    distanceSmall = playerDistance;
+                                    closestBlock = block;
+                                }
+                            }
+                    }
+                    if (closestBlock == null)
+                        continue;
+                    item.setVelocity(closestBlock.getLocation().toVector().subtract(item.getLocation().toVector()).normalize());
+                }
+            }
+        }
     }
 
 }
--- src/main/java/xyz/vertex101/hoppermagnet/items/ItemHopMagnet.java
+++ src/main/java/xyz/vertex101/hoppermagnet/items/ItemHopMagnet.java
@@ -1,7 +1,10 @@
 package xyz.vertex101.hoppermagnet.items;
 
 import org.bukkit.*;
-import org.bukkit.inventory.ItemFlag;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -9,14 +12,27 @@ import xyz.vertex101.hoppermagnet.HopperMagnet;
 
 import static xyz.vertex101.hoppermagnet.utils.TextUtils.msgFormat;
 
-public class ItemHopMagnet {
+public class ItemHopMagnet implements Listener {
     public static final ItemStack magHopper = new ItemStack(Material.HOPPER,1);
 
     public static NamespacedKey key = new NamespacedKey(HopperMagnet.getPlugin(), "magnet_hopper");
 
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if(e.getBlockPlaced().getType() == Material.HOPPER) {
+            HopperMagnet.getPlugin().magnetBlocks.add(e.getBlock());
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        if(e.getBlock().getType() == Material.HOPPER) {
+            HopperMagnet.getPlugin().magnetBlocks.remove(e.getBlock());
+        }
+    }
+
     public static void magnetHopper() {
         ItemMeta magHop = magHopper.getItemMeta();
-        magHop.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         magHop.setDisplayName(msgFormat("&4&l&nMagnet Hopper"));
         magHopper.setItemMeta(magHop);
         magHopper.getMaxStackSize();
